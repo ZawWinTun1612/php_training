329f574f93b049fdeec3c7f45f71b7498df1beb9
ZawWinTun <68418489+ZawWinTun1612@users.noreply.github.com>
2021-10-28 09:40:04 +0630

    html comment deleted

--- laravel/quickstart/resources/views/tasks.blade.php
+++ laravel/quickstart/resources/views/tasks.blade.php
@@ -9,14 +9,11 @@
                 </div>
 
                 <div class="panel-body">
-                    <!-- Display Validation Errors -->
                     @include('common.errors')
 
-                    <!-- New Task Form -->
                     <form action="{{ url('task')}}" method="POST" class="form-horizontal">
                         {{ csrf_field() }}
 
-                        <!-- Task Name -->
                         <div class="form-group">
                             <label for="task-name" class="col-sm-3 control-label">Task</label>
 
@@ -25,7 +22,6 @@
                             </div>
                         </div>
 
-                        <!-- Add Task Button -->
                         <div class="form-group">
                             <div class="col-sm-offset-3 col-sm-6">
                                 <button type="submit" class="btn btn-default">
@@ -37,7 +33,6 @@
                 </div>
             </div>
 
-            <!-- Current Tasks -->
             @if (count($tasks) > 0)
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -55,7 +50,6 @@
                                     <tr>
                                         <td class="table-text"><div>{{ $task->name }}</div></td>
 
-                                        <!-- Task Delete Button -->
                                         <td>
                                             <form action="{{ url('task/'.$task->id) }}" method="POST">
                                                 {{ csrf_field() }}
